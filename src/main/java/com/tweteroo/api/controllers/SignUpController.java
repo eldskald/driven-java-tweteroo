0ded41e45241cb7bfe10828eb173322effe7cb96
rafael <rafaelbordoni00@gmail.com>
2023-04-03 01:29:42 -0300

    change POST /sign-up return to Ok instead of Created

--- src/main/java/com/tweteroo/api/controllers/SignUpController.java
+++ src/main/java/com/tweteroo/api/controllers/SignUpController.java
@@ -1,11 +1,9 @@
 package com.tweteroo.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dtos.AccountDTO;
@@ -21,7 +19,6 @@ public class SignUpController {
     private SignUpService service;
 
     @PostMapping()
-    @ResponseStatus(HttpStatus.CREATED)
     public void signUp(@RequestBody @Valid AccountDTO body) {
         service.signUp(body);
     }
